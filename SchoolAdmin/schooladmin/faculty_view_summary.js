c7d87a6fe15d3fa47b002fbe2e25ea5e14de1a6c
MakieTech <121760681+astigPree@users.noreply.github.com>
2024-11-17 05:19:57 +0800

    add logic

--- SchoolAdmin/schooladmin/faculty_view_summary.js
+++ SchoolAdmin/schooladmin/faculty_view_summary.js
@@ -110,30 +110,67 @@ var performanceChart = new Chart(ctxPerformance, {
 
 };
 
-    
-    // Modal logic
-        const logoutButton = document.getElementById('logoutLink');  // Logout button
-        const logoutModal = document.getElementById('logoutModal');
-        const yesButton = document.querySelector('.yes-btn');
-        const noButton = document.querySelector('.no-btn');
-        
-        // Show modal when logout is clicked
-        logoutButton.addEventListener('click', function(event) {
-            event.preventDefault();  // Prevent default logout behavior
-            logoutModal.classList.remove('hidden');  // Show modal by removing 'hidden' class
-        });
-        
-        // Hide modal when "No" is clicked
-        noButton.addEventListener('click', function() {
-            logoutModal.classList.add('hidden');  // Hide modal by adding 'hidden' class
-        });
-        
-        // Redirect when "Yes" is clicked
-        yesButton.addEventListener('click', async function() {
+
+// Modal logic
+const logoutButton = document.getElementById('logoutLink');  // Logout button
+const logoutModal = document.getElementById('logoutModal');
+const yesButton = document.querySelector('.yes-btn');
+const noButton = document.querySelector('.no-btn');
+
+// Show modal when logout is clicked
+logoutButton.addEventListener('click', function(event) {
+    event.preventDefault();  // Prevent default logout behavior
+    logoutModal.classList.remove('hidden');  // Show modal by removing 'hidden' class
+});
+
+// Hide modal when "No" is clicked
+noButton.addEventListener('click', function() {
+    logoutModal.classList.add('hidden');  // Hide modal by adding 'hidden' class
+});
+
+// Redirect when "Yes" is clicked
+yesButton.addEventListener('click', async function() {
+    try {
+
+    const response = await fetch('https://bnahs.pythonanywhere.com/api/user/logout/', {
+        method: 'POST',
+        headers: {
+            'X-Requested-With': 'XMLHttpRequest',
+            
+        },
+        credentials: 'include',
+    });
+
+    const data = await response.json();
+    if (response.ok) {
+        console.log("Success Data : ", data); 
+        window.location.href = '../../get-started.html'; 
+    } else {
+        console.log("Error Data : ", data);
+    }
+    } catch (error) {
+    console.error("Error during fetch:", error);
+    }
+});
+
+
+
+
+
+
+
+
+const totalTeacher = document.getElementById('totalTeacher');
+const evaluationConducted = document.getElementById('evaluationConducted');
+const totalPending = document.getElementById('totalPending');
+
+
+
+async function fetchSummary() {
     try {
         
-        const response = await fetch('https://bnahs.pythonanywhere.com/api/user/logout/', {
-            method: 'POST',
+        const response = await fetch('https://bnahs.pythonanywhere.com/api/school/summary/', {
+            method: 'GET',
             headers: {
                 'X-Requested-With': 'XMLHttpRequest',
                 
@@ -143,12 +180,19 @@ var performanceChart = new Chart(ctxPerformance, {
 
         const data = await response.json();
         if (response.ok) {
-            console.log("Success Data : ", data); 
-            window.location.href = '../../get-started.html'; 
+            console.log("Success Data : ", data);
+            
+            totalTeacher.textContent = data.teacher_count;
+            evaluationConducted.textContent = data.evaluated_teacher_count;
+            totalPending.textContent = data.un_evaluated_teacher_count;
         } else {
             console.log("Error Data : ", data);
         }
     } catch (error) {
         console.error("Error during fetch:", error);
     }
-});
+}
+
+fetchSummary();
+
+
