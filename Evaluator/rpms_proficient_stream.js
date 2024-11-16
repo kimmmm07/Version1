522d9375ade127c217a3c1af6a2df693ea20d13e
MakieTech <121760681+astigPree@users.noreply.github.com>
2024-11-16 17:53:30 +0800

    fix proficient steam

--- Evaluator/rpms_proficient_stream.js
+++ Evaluator/rpms_proficient_stream.js
@@ -71,10 +71,110 @@ const plusFactorPosted = document.getElementById("plus-factor-posted");
 const plusFactorMenuDots = document.getElementById("plus-factor-menu-dots");
 
 
-// Event listeners
-closeButton.addEventListener('click', () => {
-    floatingMenu.classList.toggle('active');
-});
+// // Event listeners
+// closeButton.addEventListener('click', () => {
+//     floatingMenu.classList.toggle('active');
+// });
+
+
+ 
+
+
+let kra_1_id = undefined;
+let kra_2_id = undefined;
+let kra_3_id = undefined;
+let kra_4_id = undefined;
+let plus_factor_id = undefined;
+
+function onclickKRA1(){
+    sessionStorage.setItem('kra_1_id', kra_1_id);
+    window.location.href = 'rpms_proficient_kra1.html';
+}
+
+function onclickKRA2(){
+    sessionStorage.setItem('kra_2_id', kra_2_id);
+    window.location.href = 'rpms_proficient_kra2.html';
+}
+
+function onclickKRA3(){
+    sessionStorage.setItem('kra_3_id', kra_3_id);
+    window.location.href = 'rpms_proficient_kra3.html';
+}
+
+function onclickKRA4(){
+    sessionStorage.setItem('kra_4_id', kra_4_id);
+    window.location.href = 'rpms_proficient_kra4.html';
+}
+
+function onclickPlusFactor(){
+    sessionStorage.setItem('plus_factor_id', plus_factor_id);
+    window.location.href = 'rpms_proficient_plusfactor.html';
+}
+
+let folder = undefined;
+async function getClassworks(){
+    try {
+        
+        const rpms_folder_id = sessionStorage.getItem('rpms_folder_id'); // 
+
+        const formData = new FormData();
+        formData.append('rpms_folder_id', rpms_folder_id);
+        formData.append('folder_type', 'proficient' );
+
+        
+        const response = await fetch('https://bnahs.pythonanywhere.com/api/evaluator/school/get/rpms/folder/', {
+            method: 'POST',
+            headers: {
+                'X-Requested-With': 'XMLHttpRequest',
+                
+            },
+            credentials: 'include',
+            body: formData,
+        });
+
+        folder = await response.json();
+
+        if (response.ok) {
+            console.log("Success Data : ", folder);  
+
+            folder.rpms_classworks.forEach(classwork => {
+                if (classwork.title == "PLUS FACTOR") {
+                    plus_factor_id = classwork.class_work_id;
+                } else if (classwork.title == "KRA 4:  Curriculum and Planning & Assessment and Reporting") {
+                    kra_4_id = classwork.class_work_id;
+                } else if (classwork.title == "KRA 3: Curriculum and Planning") {
+                    kra_3_id = classwork.class_work_id;
+                } else if (classwork.title == "KRA 2: Learning Environment and Diversity of Learners") {
+                    kra_2_id = classwork.class_work_id;
+                } else if (classwork.title == "KRA 1: Content Knowledge and Pedagogy") {
+                    kra_1_id = classwork.class_work_id;
+                }
+            })
+
+        } else {
+            console.log("Error Data : ", folder);
+        }
+    } catch (error) {
+        console.error("Error during fetch:", error);
+    }
+}
+
+
+getClassworks();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
