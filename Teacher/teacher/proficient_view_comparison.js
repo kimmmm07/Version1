37c8c8f54ce7bd39c7acf3bbb1e6761016e0c17f
mjnavarette <147477360+mjnavarette@users.noreply.github.com>
2024-12-02 21:10:51 +0800

    Add files via upload

---
+++ Teacher/teacher/proficient_view_comparison.js
@@ -0,0 +1,82 @@
+// Objective 1 Variables
+const efficiency1Self = document.getElementById("Efficiency1_5");
+const evaluatorEfficiency1 = document.getElementById("EvaluatorEfficiency1_5");
+
+// Objective 2 Variables
+const efficiency2Self = document.getElementById("Efficiency2_5");
+const evaluatorEfficiency2 = document.getElementById("EvaluatorEfficiency2_5");
+
+// Objective 3 Variables
+const efficiency3Self = document.getElementById("Efficiency3_5");
+const evaluatorEfficiency3 = document.getElementById("EvaluatorEfficiency3_5");
+
+// Objective 4 Variables
+const efficiency4Self = document.getElementById("Efficiency4_5");
+const evaluatorEfficiency4 = document.getElementById("EvaluatorEfficiency4_5");
+
+// Objective 5 Variables
+const efficiency5Self = document.getElementById("Efficiency5_5");
+const evaluatorEfficiency5 = document.getElementById("EvaluatorEfficiency5_5");
+
+// Objective 6 Variables
+const efficiency6Self = document.getElementById("Efficiency6_5");
+const evaluatorEfficiency6 = document.getElementById("EvaluatorEfficiency6_5");
+
+// Objective 7 Variables
+const efficiency7Self = document.getElementById("Efficiency7_5");
+const evaluatorEfficiency7 = document.getElementById("EvaluatorEfficiency7_5");
+
+// Objective 8 Variables
+const efficiency8Self = document.getElementById("Efficiency8_5");
+const evaluatorEfficiency8 = document.getElementById("EvaluatorEfficiency8_5");
+
+// Objective 9 Variables
+const quality9Self = document.getElementById("Quality9_5");
+const evaluatorQuality9 = document.getElementById("EvaluatorQuality9_5");
+const efficiency9Self = document.getElementById("Efficiency9_5");
+const evaluatorEfficiency9 = document.getElementById("EvaluatorEfficiency9_5");
+
+// Objective 10 Variables
+const efficiency10Self = document.getElementById("Efficiency10_5");
+const evaluatorEfficiency10 = document.getElementById("EvaluatorEfficiency10_5");
+
+// Objective 11 Variables
+const quality11Self = document.getElementById("Quality11_5");
+const evaluatorQuality11 = document.getElementById("EvaluatorQuality11_5");
+const timeliness11Self = document.getElementById("Timeliness11_5");
+const evaluatorTimeliness11 = document.getElementById("EvaluatorTimeliness11_5");
+
+// Objective 12 Variables
+const quality12Self = document.getElementById("Quality12_5");
+const evaluatorQuality12 = document.getElementById("EvaluatorQuality12_5");
+const timeliness12Self = document.getElementById("Timeliness12_5");
+const evaluatorTimeliness12 = document.getElementById("EvaluatorTimeliness12_5");
+
+// Objective 13 Variables
+const quality13Self = document.getElementById("Quality13_5");
+const evaluatorQuality13 = document.getElementById("EvaluatorQuality13_5");
+const timeliness13Self = document.getElementById("Timeliness13_5");
+const evaluatorTimeliness13 = document.getElementById("EvaluatorTimeliness13_5");
+
+// Objective 14 Variables
+const quality14Self = document.getElementById("Quality14_5");
+const evaluatorQuality14 = document.getElementById("EvaluatorQuality14_5");
+const efficiency14Self = document.getElementById("Efficiency14_5");
+const evaluatorEfficiency14 = document.getElementById("EvaluatorEfficiency14_5");
+const timeliness14Self = document.getElementById("Timeliness14_5");
+const evaluatorTimeliness14 = document.getElementById("EvaluatorTimeliness14_5");
+
+// PLUS FACTOR Objective 15 Variables
+const quality15Self = document.getElementById("Quality15_5");
+const evaluatorQuality15 = document.getElementById("EvaluatorQuality15_5");
+const efficiency15Self = document.getElementById("Efficiency15_5");
+const evaluatorEfficiency15 = document.getElementById("EvaluatorEfficiency15_5");
+const timeliness15Self = document.getElementById("Timeliness15_5");
+const evaluatorTimeliness15 = document.getElementById("EvaluatorTimeliness15_5");
+
+// Adjectival Ratings Variables
+const teacherAverageScore = document.getElementById("teacher-average-score");
+const teacherRating = document.getElementById("teacher-rating");
+
+const evaluatorAverageScore = document.getElementById("evaluator-average-score");
+const evaluatorRating = document.getElementById("evaluator-rating");
