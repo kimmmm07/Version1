fbd979a592c34690f9fa8f6f7ed41d5a0baec6ec
MakieTech <121760681+astigPree@users.noreply.github.com>
2024-12-04 02:16:17 +0800

    push ko lang to

--- Evaluator/rpms_proficient_attachment_objective15.js
+++ Evaluator/rpms_proficient_attachment_objective15.js
@@ -18,6 +18,9 @@ const plusfactorScoreScore = document.getElementById('plusfactorScore');
 const score15 = document.getElementById('score15');
 const maxScore15 = document.getElementById('max-score15');
 
+
+let realScore = 1;
+
 window.addEventListener("load", function() {
   const savedValue = localStorage.getItem("obj15");
   if (savedValue) {
@@ -27,8 +30,116 @@ window.addEventListener("load", function() {
   const inputElement = document.getElementById("score15");
   if (inputElement) {
       inputElement.addEventListener("input", function() {
-          const inputValue = inputElement.value;
-          localStorage.setItem("obj15", inputValue);  
+        realScore = inputElement.value; 
+        localStorage.setItem("obj15", realScore);  
       });
   }
 });
+
+
+
+
+
+
+const class_work_id = sessionStorage.getItem('plus_factor_id');
+console.log(class_work_id);
+const teacher_id = sessionStorage.getItem('teacher_id');
+console.log(teacher_id);
+let teacher_name = undefined;
+
+
+let teacher = undefined;
+let submitted = undefined;
+ 
+
+async function getTeacherAttachments() {
+    try {
+
+        const formData = new FormData();
+        formData.append('class_work_id ', class_work_id);
+        formData.append('teacher_id', teacher_id);
+
+
+        const response = await fetch('https://bnahs.pythonanywhere.com/api/evaluator/school/get/rpms/folder/classwork/attachments/', {
+            method: 'POST',
+            headers: {
+                'X-Requested-With': 'XMLHttpRequest',
+                
+            },
+            credentials: 'include',
+            body: formData,
+        });
+
+        const data = await response.json();
+        if (response.ok) {
+            console.log("Success Data : ", data);  
+            teacher = data.teacher;
+            submitted = data.submitted?.[0];
+            
+            console.log(teacher);
+            console.log(submitted);
+
+
+
+
+        } else {
+            console.log("Error Data : ", data);
+        }
+    } catch (error) {
+        console.error("Error during fetch:", error);
+    }
+}
+
+getTeacherAttachments();
+
+
+
+
+
+
+async function sendAnswer() {
+    try{
+        const formData = new FormData();
+        let grade = submitted.grade;
+        grade['15']['Score'] = realScore;
+        console.log(grade);
+        
+        formData.append('rpms_id', submitted.attachment_id);
+        formData.append('content', JSON.stringify(grade));
+        formData.append('index', "1");
+
+        const comment = document.getElementById('private-comments-textarea').value;
+        formData.append('comment', comment);
+
+
+        const response = await fetch('https://bnahs.pythonanywhere.com/api/evaluator/school/check/rpms/attachment/', {
+            method: 'POST',
+            headers: {
+                'X-Requested-With': 'XMLHttpRequest',
+                
+            },
+            credentials: 'include',
+            body: formData,
+        });
+
+        const data = await response.json();
+        if (response.ok) {
+            console.log("Success Data : ", data);   
+        } else {
+            console.log("Error Data : ", data);
+        }
+
+    } catch (error) {
+        console.error("Error during fetch:", error);
+    }
+}
+
+
+returnBtn15.addEventListener('click', async() => {
+    await sendAnswer();
+    setTimeout(() => {
+        window.location.href = 'rpms_proficient_tw_plusfactor.html';
+    })
+});
+
+
