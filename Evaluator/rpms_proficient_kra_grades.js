b0513c3bf461c2b757cadbd7e6414f1762356613
mjnavarette <147477360+mjnavarette@users.noreply.github.com>
2024-12-04 16:09:47 +0800

    --

--- Evaluator/rpms_proficient_kra_grades.js
+++ Evaluator/rpms_proficient_kra_grades.js
@@ -1,43 +1,6 @@
-// Report Table Titles
-const objective1Title = document.getElementById("objective1Title");
-const objective2Title = document.getElementById("objective2Title");
-const objective3Title = document.getElementById("objective3Title");
-const objective4Title = document.getElementById("objective4Title");
-const objective5Title = document.getElementById("objective5Title");
-const objective6Title = document.getElementById("objective6Title");
-const objective7Title = document.getElementById("objective7itle");
-const objective8Title = document.getElementById("objective8Title");
-const objective9Title = document.getElementById("objective9Title");
-const objective10Title = document.getElementById("objective10Title");
-const objective11Title = document.getElementById("objective11Title");
-const objective12itle = document.getElementById("objective12Title");
-const objective13Title = document.getElementById("objective13Title");
-const objective14Title = document.getElementById("objective14itle");
-const objective15Title = document.getElementById("objective15Title");
 
-// Report Table Scores
-const objective1Score = document.getElementById("objective1Score");
-const objective2Score = document.getElementById("objective2Score");
-const objective3Score = document.getElementById("objective3Score");
-const objective4Score = document.getElementById("objective4Score");
-const objective5Score = document.getElementById("objective4Score");
-const objective6Score = document.getElementById("objective6Score");
-const objective7Score = document.getElementById("objective7Score");
-const objective8Score = document.getElementById("objective8Score");
-const objective9Score = document.getElementById("objective9Score");
-const objective10Score = document.getElementById("objective10Score");
-const objective11Score = document.getElementById("objective11Score");
-const objective12Score = document.getElementById("objective12Score");
-const objective13Score = document.getElementById("objective13Score");
-const objective14Score = document.getElementById("objective14Score");
-const objective15Score = document.getElementById("objective15Score");
 
-// Report Table Total Scores
-const kra1totalScore = document.getElementById("kra1totalScore");
-const kra2totalScore = document.getElementById("kra2totalScore");
-const kra3totalScore = document.getElementById("kra3totalScore");
-const kra4totalScore = document.getElementById("kra4totalScore");
-const plusfactortotalScore = document.getElementById("plusfactortotalScore");
+
 
 // Modal logic
 const logoutButton = document.getElementById('logoutLink');  // Logout button
