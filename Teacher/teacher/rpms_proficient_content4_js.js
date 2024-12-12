e2e74764e033d2977a2d611d107d26f12480dbfa
MakieTech <121760681+astigPree@users.noreply.github.com>
2024-12-12 19:10:49 +0800

    kra 4

--- Teacher/teacher/rpms_proficient_content4_js.js
+++ Teacher/teacher/rpms_proficient_content4_js.js
@@ -187,6 +187,61 @@ async function getScoreAndMilestone() {
 
                 reflectedScoreKRA4.value = String(submit['0']['Overall Score']);
             }
+
+
+
+
+
+            const attachment = submit['0'];
+            attachment?.teacher_comments?.map((comment)=>{
+                const dateObject = new Date(comment?.date);  // Parse the date string into a Date object
+
+                // Create a date formatter
+                const options = { 
+                    month: 'numeric', 
+                    day: 'numeric', 
+                    year: 'numeric', 
+                    hour: 'numeric', 
+                    minute: 'numeric', 
+                    second: 'numeric', 
+                    hour12: true 
+                };
+                const formatter = new Intl.DateTimeFormat('en-US', options);
+
+                // Format the date
+                const formattedDate = formatter.format(dateObject);
+                const profilePicture = comment.image ? `https://bnahs.pythonanywhere.com${comment.image}` : "asset/User_Circle.png";
+
+                const commentItem = `
+                    <div class="private-comment-item">
+                        <img src="${profilePicture}" class="comment-user-icon" alt="User Icon" />
+                        <div>
+                            <div class="comment-header">
+                                <span class="comment-username">${comment.name}</span>
+                                <span class="comment-timestamp">${formattedDate}</span>
+                            </div>
+                            <div class="comment-text">${comment.comment}</div>
+                        </div>
+                    </div>
+                `;
+                
+                // Append the new comment to the comment list
+                document.querySelector('.private-comment-list').insertAdjacentHTML('afterbegin', commentItem);
+            })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         } else {
             console.error('Failed to fetch attachments:', response.statusText);
         }
