dc872feddfc483d91e95656d67496a31927c4d30
MakieTech <121760681+astigPree@users.noreply.github.com>
2024-12-18 15:45:32 +0800

    grading

--- Evaluator/rpms_proficient_kra_grades.js
+++ Evaluator/rpms_proficient_kra_grades.js
@@ -140,6 +140,7 @@ async function getTeacherAttachments() {
             }
 
         } else {
+            window.location.href = '../../get-started.html'; 
             console.log("Error Data : ", data);
         }
     } catch (error) {
